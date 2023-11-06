268724169b2e7d7fc2c1c09f509ed342428a2464
Rafael Carvalho <carvalho.rafael89@gmail.com>
2023-11-06 17:32:28 -0300

    att headers, footers, responsividade

--- Hogwarts/js/script.js
+++ Hogwarts/js/script.js
@@ -32,3 +32,21 @@ function expandir() {
     card.style.width="500px"
     card.style.heigth="500px"
 }
+
+// function voltarMenu() {
+//     window.addEventListener('scroll', () => {
+//         let menuLateral = document.querySelector('#menuLateral');
+//         let inputCheck = document.querySelector('.alteraMenu');
+//         if(window.scrollY > 1) {
+//             menuLateral.style.left = "-300px"
+//             inputCheck.checked = false;
+//         }
+//     });
+// }   
+// voltarMenu();
+
+// let menuCheck = document.querySelector('.alteraMenu');
+// menuCheck.addEventListener('click', showChecked);
+// function showChecked() {
+//     console.log(menuCheck)
+// }
